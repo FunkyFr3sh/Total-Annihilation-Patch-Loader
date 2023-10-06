96cdebacde50489fa7cbe0b1817f9fcfc0c3a1a7
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-06 11:58:39 +0200

    add macro to easily get the memory address

--- patches.c
+++ patches.c
@@ -10,9 +10,6 @@ char g_patches_debug_str[512];
 
 static int patches_apply_presets(void* user, const char* name, const char* value)
 {
-    char* text_base  = (char*)GetModuleHandleA(NULL) + 0x0C00; /* start=00000400 (mem=00401000) */
-    char* rdata_base = (char*)GetModuleHandleA(NULL) + 0x1200; /* start=000FAE00 (mem=004FC000) */
-
     if (_strcmpi(name, "RegistryPath") == 0)
     {
         if (strlen(value) >= 1 && strlen(value) <= 21)
@@ -75,8 +72,8 @@ static int patches_apply_presets(void* user, const char* name, const char* value
         {
             // Make AI commander orders NOT reset whenever attacked.
             // (00406FB4)
-            patch_setbyte(text_base + 0x000063B4, 0xEB);
-            patch_setbyte(text_base + 0x000063B5, 0x40);
+            patch_setbyte(GET_MEM_ADDRESS(0x000063B4), 0xEB);
+            patch_setbyte(GET_MEM_ADDRESS(0x000063B5), 0x40);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -92,9 +89,9 @@ static int patches_apply_presets(void* user, const char* name, const char* value
             // Change pathfinding "+search #" to higher initial number (thanks Ti_ and xpoy)
             // http://www.tauniverse.com/forum/showthread.php?t=41608&page=4
             // (0040EAC9)
-            patch_setbyte(text_base + 0x0000DED6, 0x5A);
-            patch_setbyte(text_base + 0x0000DED7, 0x04);
-            patch_setbyte(text_base + 0x0000DED8, 0x01);
+            patch_setbyte(GET_MEM_ADDRESS(0x0000DED6), 0x5A);
+            patch_setbyte(GET_MEM_ADDRESS(0x0000DED7), 0x04);
+            patch_setbyte(GET_MEM_ADDRESS(0x0000DED8), 0x01);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -107,8 +104,8 @@ static int patches_apply_presets(void* user, const char* name, const char* value
         if (_strcmpi(value, "Yes") == 0)
         {
             // DirectX popup elimination
-            patch_setbyte(text_base + 0x00025AA5, 0xB0);
-            patch_setbyte(text_base + 0x00025AA6, 0x01);
+            patch_setbyte(GET_MEM_ADDRESS(0x00025AA5), 0xB0);
+            patch_setbyte(GET_MEM_ADDRESS(0x00025AA6), 0x01);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -122,8 +119,8 @@ static int patches_apply_presets(void* user, const char* name, const char* value
         {
             // Enable cursor to be "reclaim" when reclaim cursor hovers over any unit
             // Now Commanders can be anonymous on the minimap
-            patch_setbyte(text_base + 0x0003DB86, 0x00);
-            patch_setbyte(text_base + 0x0003DB87, 0x00);
+            patch_setbyte(GET_MEM_ADDRESS(0x0003DB86), 0x00);
+            patch_setbyte(GET_MEM_ADDRESS(0x0003DB87), 0x00);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -138,7 +135,7 @@ static int patches_apply_presets(void* user, const char* name, const char* value
             // Disable "\" key from being repeat-last-typed-command
             // (such that "\" will be dedicated demo recorder whiteboard key)
             // (00496694)
-            patch_setbyte(text_base + 0x00095AE7, 0x27);
+            patch_setbyte(GET_MEM_ADDRESS(0x00095AE7), 0x27);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -151,8 +148,8 @@ static int patches_apply_presets(void* user, const char* name, const char* value
         if (_strcmpi(value, "Yes") == 0)
         {
             // Enable debug mode via F10 key. Must be in developers mode to activate.
-            patch_setbyte(text_base + 0x00095B76, 0x24);
-            patch_setbyte(text_base + 0x00095B79, 0x0D);
+            patch_setbyte(GET_MEM_ADDRESS(0x00095B76), 0x24);
+            patch_setbyte(GET_MEM_ADDRESS(0x00095B79), 0x0D);
         }
         else if (_strcmpi(value, "No") != 0)
         {
@@ -167,7 +164,7 @@ static int patches_apply_presets(void* user, const char* name, const char* value
             // Increase "+atm" to fill resources (thanks N72)
             // http://www.tauniverse.com/forum/showthread.php?t=41608&page=6
             // (004FCC7C via 004170C0 function)
-            patch_setbyte(rdata_base + 0x000FBA7F, 0xD4);
+            patch_setbyte(GET_MEM_ADDRESS(0x000FBA7F), 0xD4);
         }
         else if (_strcmpi(value, "No") != 0)
         {
--- patches.h
+++ patches.h
@@ -1,6 +1,13 @@
 #ifndef PATCHES_H 
 #define PATCHES_H 
 
+#define GET_MEM_ADDRESS(a) \
+    ((char*)GetModuleHandleA(NULL) + (a) + (\
+    (a) >= 0x0FF600 + 0x10A00 ? -(int)GetModuleHandleA(NULL) - (a) : \
+    (a) >= 0x0FF600 ? 0x1A00 : \
+    (a) >= 0x0FAE00 ? 0x1200 : \
+    (a) >= 0x000400 ? 0x0C00 : -(int)GetModuleHandleA(NULL) - (a)))
+
 extern char g_patches_debug_str[];
 
 int patches_apply(HMODULE mod);
